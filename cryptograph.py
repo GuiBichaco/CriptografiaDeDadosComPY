80b098fd39a812be1f39bd850be5c345fe20e8a5
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-05-12 15:22:26 -0300

    Importando as bibliotecas e gerando par de chaves

--- cryptograph.py
+++ cryptograph.py
@@ -1,3 +1,13 @@
 # Intalável abaixo para fazer o projeto funcionar
 # pip install cryptography
 
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+
+#1. Gerar par de chaves RSA
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+public_key = private_key.public_key()
+
